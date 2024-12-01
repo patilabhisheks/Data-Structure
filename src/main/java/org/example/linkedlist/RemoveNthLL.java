367b71458d537bd7dce6e33d075fd13689142c86
Abhishek Patil <patilabhijugul@gmail.com>
2024-12-01 12:19:48 +0530

    removeNthLL

--- src/main/java/org/example/linkedlist/RemoveNthLL.java
+++ src/main/java/org/example/linkedlist/RemoveNthLL.java
@@ -23,8 +23,8 @@ public class RemoveNthLL {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
-        int n=2;
+        int arr[] = {1, 2, 3, 4, 5,6};
+        int n=4;
         Node head = new Node(arr[0]);
         Node current = head;
 
@@ -33,30 +33,21 @@ public class RemoveNthLL {
             current.next = newNode;
             current = newNode;
         }
-        printList(head);
 
-//        Node currentHead= head;
-//
-//        Stack<Integer> temp = new Stack<>();
-//        while (currentHead!=null)
-//        {
-//            temp.push(currentHead.data);
-//            currentHead=currentHead.next;
-//        }
-//        currentHead=head;
-//        int currentPosition=0;
-//        while (currentHead!=null)
-//        {
-//            if(currentPosition==n)
-//            {
-//                break;
-//            }else {
-//                currentHead.data = temp.pop();
-//                currentHead = currentHead.next;
-//            }
-//
-//            currentPosition++;
-//        }
-//        printList(currentHead);
+        Node dummy = new Node(0);
+        dummy.next=head;
+        Node fast = dummy;
+        Node slow = dummy;
+        for(int i=0;i<n;i++)
+        {
+                fast=fast.next;
+        }
+        while (fast!=null && fast.next!=null)
+        {
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        printList(dummy.next);
     }
     }
